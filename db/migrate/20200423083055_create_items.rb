0a7238f33f5f1d2dc56dae1e074005b5775483bc
Taichi Masuyama <montanha.masu536@gmail.com>
2020-04-24 10:27:34 +0900

    カテゴリーをseedsに記入

--- db/migrate/20200423083055_create_items.rb
+++ db/migrate/20200423083055_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.string     :name,                null: false
       t.text       :description,         null: false
-      t.references :category_id,         null: false, foreign_key: true
+      t.references :category,            null: false, foreign_key: true
       t.string     :condition,           null: false
       t.string     :delivery_charge,     null: false
       t.string     :delivery_prefecture, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_082822) do
+ActiveRecord::Schema.define(version: 2020_04_23_083055) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,4 +20,20 @@ ActiveRecord::Schema.define(version: 2020_04_23_082822) do
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.bigint "category_id", null: false
+    t.string "condition", null: false
+    t.string "delivery_charge", null: false
+    t.string "delivery_prefecture", null: false
+    t.string "delivery_dates", null: false
+    t.integer "price", null: false
+    t.integer "status", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
+  add_foreign_key "items", "categories"
 end
