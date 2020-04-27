a2921297b4831d2b5ac23e742ad0f83bf26dded6
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-04-27 22:54:26 +0900

    new_registration_view

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,6 +10,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     @user = User.new(sign_up_params)
+    binding.pry
     unless @user.valid?
       flash.now[:alert] = @user.errors.full_messages
       render :new and return
