477eb988a85aba2ceba12283d79b9cab4c0f2f6e
Taichi Masuyama <montanha.masu536@gmail.com>
2020-04-28 19:14:06 +0900

    イメージモデル関係

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     get "user_show", to: "users/registrations#show"
   end
   root 'items#index'
-  resources :items, only: [:index, :new, :show, :create]
+  resources :items, only: [:index, :new, :create, :show] do
+    resources :images, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
