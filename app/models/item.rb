e62d65699de6a991ffe61033df11580359173fcd
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-04-28 20:30:42 +0900

    edit controller-action

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :images
   belongs_to :order
+  
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get "user_show", to: "users/registrations#show"
   end
   root 'items#index'
-  resources :items, only: [:index, :new, :create, :show] do
+  resources :items, only: [:index, :new, :create, :update, :show, :destroy] do
     resources :images, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
