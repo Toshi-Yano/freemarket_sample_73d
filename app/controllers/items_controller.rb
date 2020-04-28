d65d965f7f7ca2fab22915ddd0ca5137fe25dd74
Taichi Masuyama <montanha.masu536@gmail.com>
2020-04-28 12:33:40 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,4 @@
 class ItemsController < ApplicationController
   def index
-    @ladies = Item.includes(:images).where('status = ? and category_id = ?', 1, 1).order('id DESC').limit(3)
-    @mens = Item.includes(:images).where('status = ? and category_id = ?', 1, 200).order('id DESC').limit(3)
-    @electricals = Item.includes(:images).where('status = ? and category_id = ?', 1, 872).order('id DESC').limit(3)
-    @toys = Item.includes(:images).where('status = ? and category_id = ?', 1, 620).order('id DESC').limit(3)
   end
 end
