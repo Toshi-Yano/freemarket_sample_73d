88a53d59cada506c4b8e0f137caf041351752308
NOBU0321 <60180339+NOBU0321@users.noreply.github.com>
2020-04-29 14:27:21 +0900

    .

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :delivery_date
+  belongs_to_active_hash :prefecture
+  
   belongs_to :category
   belongs_to :user
   has_many :images
   belongs_to :order
+  
 end
