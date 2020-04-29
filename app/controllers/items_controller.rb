762c2262327f46cba6e092c2fa0063945883056b
NOBU0321 <60180339+NOBU0321@users.noreply.github.com>
2020-04-29 17:56:07 +0900

    .

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_category, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_item, except: [:index, :new, :create]
   
   def index
@@ -34,7 +35,23 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
   
+
+  #jsonで親の名前で検索し、紐づく小カテゴリーの配列を取得
+  def get_category_children
+    @category_children = Category.find(params[:parent_name]).children
+  end
+
+  #jsonで子カテゴリーに紐づく孫カテゴリーの配列を取得
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
+
   private
+  #親カテゴリー
+  def set_category  
+    @category_parent_array = Category.where(ancestry: nil)
+  end
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :delivery_charge, :delivery_prefecture, :delivery_dates, :price, :status, :condition, images_attributes: [:image, :_destroy, :id])
