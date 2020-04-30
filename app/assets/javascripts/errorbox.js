cbdb324e9c9cafb28e36133932ad923923622872
Taichi Masuyama <montanha.masu536@gmail.com>
2020-04-30 15:33:45 +0900

    errorbox.jsにエラー文の記述を全て移動

--- app/assets/javascripts/errorbox.js
+++ app/assets/javascripts/errorbox.js
@@ -3,7 +3,6 @@ $(function(){
     const itemNameInput = document.getElementById('item_name');
     const ebForText_array = Array.from(document.getElementsByClassName('errorbox-fortext__errormessage'));
     const ebForText = ebForText_array[0];
-    itemNameInput.classList.add('input_type_text');
     itemNameInput.addEventListener('blur', () => {
       if (itemNameInput.value === "") {
         itemNameInput.classList.add('not-selected');
@@ -21,7 +20,6 @@ $(function(){
     const itemDescriptionInput = document.getElementById('item_description');
     const ebForTextarea_array = Array.from(document.getElementsByClassName('errorbox-fortextarea__errormessage'));
     const ebForTextarea = ebForTextarea_array[0];
-    itemDescriptionInput.classList.add('input_type_text');
     itemDescriptionInput.addEventListener('blur', () => {
       if (itemDescriptionInput.value === "") {
         itemDescriptionInput.classList.add('not-selected');
@@ -51,6 +49,21 @@ $(function(){
           error_array[index].classList.remove('errorbox__errormessage__true');
         }
       });
+    });
+
+    const itemPriceInput = document.getElementById('item_price');
+    const ebForPrice_array = Array.from(document.getElementsByClassName('errorbox-forprice__errormessage'));
+    const ebForPrice = ebForPrice_array[0];
+    let i = 0;
+    itemPriceInput.addEventListener('blur', () => {
+      if (itemPriceInput.value < 300 || itemPriceInput.value > 9999999) {
+        itemPriceInput.classList.add('not-selected');
+        ebForPrice.classList.add('errorbox-forprice__errormessage__true');
+        i++;
+      } else {
+        itemPriceInput.classList.remove('not-selected');
+        ebForPrice.classList.remove('errorbox-forprice__errormessage__true');
+      }
     });
   })();
 })
