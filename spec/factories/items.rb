8c87707408a710fc7caed74b0552aa93cac18070
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-05-01 11:39:38 +0900

    add rspec

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,14 +4,12 @@ FactoryBot.define do
     name                {"ジャケット"}
     description         {"かっこいいジャケットです"}
     category
-    delivery_charge {1}
-    prefecture {1}
-    delivery_dates {1}
-    condition {1}
+    delivery_charge_id  {1}
+    prefecture_id       {1}
+    delivery_dates_id   {1}
+    condition_id        {1}
     price               {5000}
     status              {1}
-    # payer               {"購入者"}
-    # brand               {"ユニクロ"}
     after(:build) do |item|                          
       item.images << build(:image, item: item)
     end
