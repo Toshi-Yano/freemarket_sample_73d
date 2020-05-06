ecc97e2b025dfb5eef3ff8ae620b17070b5a8daf
Taichi Masuyama <montanha.masu536@gmail.com>
2020-05-06 19:07:02 +0900

    ルーティングを整えました

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {
     registrations: "users/registrations",
   }
@@ -10,18 +11,12 @@ Rails.application.routes.draw do
     get "user_destroy", to: "users/sessions#index"
   end
   root 'items#index'
-  resources :items
-  resources :items, only: [:index, :new, :create, :update, :show, :destroy] do
+  resources :items do
     resources :images, only: [:new, :create]
-  end
-
-  resources :items, only: [:index, :show, :new, :edit, :destroy] do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
     end
   end
-    
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
