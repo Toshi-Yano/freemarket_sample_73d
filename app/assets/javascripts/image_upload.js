d76952e259f0ba9e8fc6c6e26c980916dba6bc4c
Taichi Masuyama <montanha.masu536@gmail.com>
2020-05-06 15:29:16 +0900

    編集によるinput追加を阻止

--- app/assets/javascripts/image_upload.js
+++ app/assets/javascripts/image_upload.js
@@ -4,7 +4,7 @@ $(function(){
       const html = `<div class="js-file_group" data-index="${index}">
                       <div class="preview-none">
                         <div class="js-image-zone"></div>
-                        <div class="js-edit">編集</div>
+                        <div class="js-edit">変更</div>
                         <div class="js-remove">削除</div>
                       </div>
                       <label class="input">
@@ -52,33 +52,37 @@ $(function(){
 
     $('#image-box').on('change', '.js-file', function(e) {
       const targetIndex = $(this).parent().parent().data('index');
-      console.log(targetIndex);
       const file = e.target.files[0];
       const blobUrl = window.URL.createObjectURL(file);
       if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-        console.log(img);
-        console.log("あわわ");
         img.setAttribute('src', blobUrl);
       } else {
         const $imageZone = $('.js-image-zone').eq(targetIndex);
-        console.log("ぱんつ");
         $imageZone.append(buildImg(targetIndex, blobUrl));
       }
 
-      $(this).parent().prev().addClass('preview');
-      $(this).parent().css('display', 'none');
-      $('.js-remove').addClass('visible-js-remove')
-      if ($('.visible-js-remove').length < 10) {
-        $('#image-box').append(buildFileField(fileIndex[0]));
-        fileIndex.shift();
-        fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
-        const num = $('.visible-js-remove').length;
-        const left = 10 - num;
-        $('#how-many-image').children().remove();
-        $('#how-many-image').append(`<p style="color:#0095ee">あと${left}枚アップロードできます</p>`);
+      console.log(fileIndex);
+      console.log(targetIndex + 1);
+      console.log($.inArray(targetIndex + 1, fileIndex));
+
+      if ( $.inArray(targetIndex + 1, fileIndex) == 0) {
+        $(this).parent().prev().addClass('preview');
+        $(this).parent().css('display', 'none');
+        $('.js-remove').addClass('visible-js-remove')
+        if ($('.visible-js-remove').length < 10) {
+          $('#image-box').append(buildFileField(fileIndex[0]));
+          fileIndex.shift();
+          fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+          const num = $('.visible-js-remove').length;
+          const left = 10 - num;
+          $('#how-many-image').children().remove();
+          $('#how-many-image').append(`<p style="color:#0095ee">あと${left}枚アップロードできます</p>`);
+        } else {
+          $('#how-many-image').children().remove();
+          $('#how-many-image').append(`<p style="color:red">これ以上アップロードできません</p>`);
+        }
       } else {
-        $('#how-many-image').children().remove();
-        $('#how-many-image').append(`<p style="color:red">これ以上アップロードできません</p>`);
+        ;
       }
     });
 
