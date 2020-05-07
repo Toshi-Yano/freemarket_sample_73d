f21206777599a204230cd82c1fb3bc7fc9e466eb
Taichi Masuyama <montanha.masu536@gmail.com>
2020-05-07 13:26:03 +0900

    エラーメッセージ表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
 
   validates :name, presence: true, length: {maximum: 40}
   validates :description, presence: true, length: {maximum: 1000}
-  validates :category_id, presence: true
+  # validates :category_id, presence: true
   validates :condition_id, presence: true
   validates :delivery_charge_id, presence: true
   validates :prefecture_id, presence: true
